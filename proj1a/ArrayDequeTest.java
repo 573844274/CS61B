65f0b3a021c8f9b029b2f685260ebb1d2e8474cb
zhanglk6 <573844274@qq.com>
2021-04-03 09:08:41 +0800

    proj1a completed

---
+++ proj1a/ArrayDequeTest.java
@@ -0,0 +1,73 @@
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotEquals;
+
+import org.junit.Test;
+public class ArrayDequeTest {
+    @Test
+    public void testConstructor() {
+        ArrayDeque<Integer> one = new ArrayDeque<Integer>(1);
+        int exp = 1;
+        int input = one.get(0);
+        assertEquals(exp, input);
+        ArrayDeque<Integer> none = new ArrayDeque<Integer>();
+        assertTrue(none.isEmpty());
+    }
+    @Test
+    public void testAdd() {
+        ArrayDeque<Integer> a1 = new ArrayDeque<Integer>(1);
+        a1.addFirst(0);
+        a1.addLast(2);
+        a1.addLast(3);
+        int exp = 3;
+        int input = a1.get(3);
+        assertEquals(exp, input);
+        int size = 4;
+        assertEquals(size,a1.size());
+        assertNull(a1.get(5));
+    }
+    @Test
+    public void restRemove() {
+        ArrayDeque<Integer> a1 = new ArrayDeque<Integer>(1);
+        a1.addFirst(0);
+        a1.addLast(2);
+        a1.removeFirst();
+        a1.removeLast();
+        a1.addFirst(0);
+        a1.addLast(2);
+        a1.addLast(3);
+        a1.printDeque();
+        int zero = a1.removeFirst();
+        int three = a1.removeLast();
+        assertEquals(0, zero);
+        assertEquals(3, three);
+        int one = a1.get(0);
+        int two = a1.get(1);
+        assertEquals(1,one);
+        assertEquals(2, two);
+        assertEquals(2, a1.size());
+        a1.printDeque();
+    }
+
+    @Test
+    public void testResize() {
+        ArrayDeque<Integer> a1 = new ArrayDeque<Integer>(1);
+        a1.addFirst(0);
+        a1.addLast(2);
+        a1.addLast(3);
+        a1.addLast(4);
+        a1.addLast(5);
+        a1.addLast(6);
+        a1.addLast(7);
+        a1.addLast(8);
+        a1.addLast(9);
+        for (int i = 0;i < 8; i += 1){
+            a1.removeLast();
+        }
+        int zero = a1.get(0);
+        int one = a1.get(1);
+        int two = a1.size();
+        assertEquals(0, zero);
+        assertEquals(1, one);
+        assertEquals(2, two);
+    }
+}
---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,120 @@
+public class LinkedListDeque<T> {
+
+    public class TNode {
+        private T item;
+        private TNode next;
+        private TNode back;
+
+        public TNode(T i, TNode n, TNode b) {
+            item = i;
+            next = n;
+            back = b;
+        }
+    }
+
+    private TNode sentiniel;
+
+    {
+        sentiniel = new TNode(null, null, null);
+        sentiniel.next = sentiniel;
+        sentiniel.back = sentiniel;
+    }
+    private int size = 0;
+
+    public LinkedListDeque() {
+        sentiniel.next = new TNode(null, sentiniel, sentiniel);
+        sentiniel.back = sentiniel.next;
+    }
+
+    public LinkedListDeque(T x) {
+        sentiniel.next = new TNode(x, sentiniel, sentiniel);
+        sentiniel.back = sentiniel.next;
+        size += 1;
+    }
+
+    public void addFirst(T item) {
+        /* Adds an item of type T to the front of the deque. */
+        TNode replacement;
+        replacement = new TNode(item, sentiniel.next, sentiniel);
+        replacement.next.back = replacement;
+        sentiniel.next = replacement;
+        size += 1;
+    }
+
+    public void addLast(T item) {
+        /* Adds an item of type T to the back of the deque. */
+        TNode replacement;
+        replacement = new TNode(item, sentiniel, sentiniel.back);
+        replacement.back.next = replacement;
+        sentiniel.back = replacement;
+        size += 1;
+    }
+
+    public boolean isEmpty() {
+        /* Returns true if deque is empty, false otherwise. */
+        return (size == 0);
+    }
+
+    public int size() {
+        /* Returns the number of items in the deque. */
+        return size;
+    }
+
+    public void printDeque() {
+        /* Prints the items in the deque from first to last, separated by a space. */
+        TNode p = sentiniel.next;
+        while (p != sentiniel) {
+            System.out.print(p.item);
+            p = p.next;
+        }
+    }
+
+    public T removeFirst() {
+        /* Removes and returns the item at the front of the deque.
+         * If no such item exists, returns null. */
+        T removeItem = sentiniel.next.item;
+        sentiniel.next = sentiniel.next.next;
+        sentiniel.next.back = sentiniel;
+        size -= 1;
+        return removeItem;
+    }
+
+    public T removeLast() {
+        /* Removes and returns the item at the back of the deque.
+         * If no such item exists, returns null. */
+        T removeItem = sentiniel.back.item;
+        sentiniel.back = sentiniel.back.back;
+        sentiniel.back.next = sentiniel;
+        size -= 1;
+        return removeItem;
+    }
+
+    public T get(int index) {
+        /* Gets the item at the given index, where 0 is the front,
+         * 1 is the next item, and so forth.
+         * If no such item exists, returns null. Must not alter the deque! */
+        TNode p = sentiniel.next;
+        for (int i = 0; i < index; i++) {
+            p = p.next;
+        }
+        return p.item;
+    }
+
+    public T  helpGetRecursive(TNode p, int index) {
+        if (index == 0) {
+            return p.item;
+        } else {
+            return helpGetRecursive(p.next, index - 1);
+        }
+    }
+    public T getRecursive(int index) {
+        if (index >= size) {
+            return null;
+        } else {
+            return helpGetRecursive(sentiniel.next,index);
+        }
+    }
+}
+
+
+
